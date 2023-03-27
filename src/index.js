d3f38e5d9d6f4164f612494e0b830836d2259bbf
UA-Viktor <v.korenkov@icloud.com>
2023-03-27 21:00:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import menuburger from './js/menu-burger';
+// import menuburger from './js/menu-burger';
 // import themeSwitcher from './js/theme-switcher';
 
-menuburger();
+// menuburger();
 // themeSwitcher();
+
+import './js/menu-burger';
 
 import './js/theme-switcher';
